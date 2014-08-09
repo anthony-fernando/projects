b01a2bb4c9afda0a25ffaca4a767a014847b97ef
Shouvik Mukherjee <contact@ishouvik.com>
2014-08-09 21:53:36 +0530

    JS moved to footer and AJAX content

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,11 @@
 // Main JS File
+
+
+// Load remote page/example.html
+$(function() {
+	$("#remote-page").click(function() {
+		$("#remote-content").load("pages/example.html", function() {
+			alert('Loaded remote content via ajax!');
+		});
+	});
+})
